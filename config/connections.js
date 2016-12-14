8fb995627fa9483ead75bed9f04e59ac156ae777
paladinze <paladinze@hotmail.com>
2016-12-14 17:25:52 -0500

    removed extra attribute from connection

--- config/connections.js
+++ config/connections.js
@@ -1,12 +1,10 @@
 
-
+//change the params below to match your local server setting
 module.exports.connections = {
-
   localPostgresqlServer: {
     user: 'postgres',
     password: 'postgres',
     database: 'test',
-    dialect: 'postgres',
     options: {
       dialect: 'postgres',
       host   : 'localhost',
