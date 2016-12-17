88564651c4e426785a0ff73c20cffac0ceb7c1e9
paladinze <paladinze@hotmail.com>
2016-12-17 11:21:17 -0500

    connection: added default model options

--- config/connections.js
+++ config/connections.js
@@ -6,10 +6,18 @@ module.exports.connections = {
     password: 'postgres',
     database: 'test',
     options: {
+      //db options
       dialect: 'postgres',
       host   : 'localhost',
       port   : 5432,
-      logging: console.log
+      logging: console.log,
+
+      //model options
+      timestamps: true, //enable timestamps for all tables
+      underscored: true, // use underscored naming style
+      paranoid: false, // perform actual delete
+      freezeTableName: true, // stop sequelize from transforming tableName
+
     }
   }
 };
