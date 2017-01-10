12fa2f47df24ec129b4a09808cf85f475096ff1b
paladinze <paladinze@hotmail.com>
2017-01-10 13:15:15 -0500

    added base policies for controllers

--- config/policies.js
+++ config/policies.js
@@ -8,58 +8,72 @@
  *
  * Any policy file (e.g. `api/policies/authenticated.js`) can be accessed
  * below by its filename, minus the extension, (e.g. "authenticated")
- *
- * For more information on how policies work, see:
- * http://sailsjs.org/#!/documentation/concepts/Policies
- *
- * For more information on configuring policies, check out:
- * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.policies.html
  */
 
 
 module.exports.policies = {
-
-  /***************************************************************************
-   *                                                                          *
-   * Default policy for all controllers and actions (`true` allows public     *
-   * access)                                                                  *
-   *                                                                          *
-   ***************************************************************************/
-
-  // '*': true,
-
-  /***************************************************************************
-   *                                                                          *
-   * Here's an example of mapping some policies to run before a controller    *
-   * and its actions                                                          *
-   *                                                                          *
-   ***************************************************************************/
-  // RabbitController: {
-
-  // Apply the `false` policy as the default for all of RabbitController's actions
-  // (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
-  // '*': false,
-
-  // For the action `nurture`, apply the 'isRabbitMother' policy
-  // (this overrides `false` above)
-  // nurture	: 'isRabbitMother',
-
-  // Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
-  // before letting any users feed our rabbits
-  // feed : ['isNiceToAnimals', 'hasRabbitFood']
-  // }
-
 
   //remember to enable this for jwt
   //'*': ['isAuthorized'], // Everything resctricted by default
   AuthController: {
     '*': true
   },
-
+  CommentController: {
+    'find': true,
+    'findOne': true,
+    '*': false
+  },
+  CourseController: {
+    'find': true,
+    'findOne': true,
+    '*': false
+  },
+  CourseStatController: {
+    'find': true,
+    'findOne': true,
+    '*': false
+  },
+  DeptController: {
+    'find': true,
+    'findOne': true,
+    '*': false
+  },
+  ElectiveController: {
+    'find': true,
+    'findOne': true,
+    '*': false
+  },
+  PositionController: {
+    'find': true,
+    'findOne': true,
+    '*': false
+  },
+  ProfController: {
+    'find': true,
+    'findOne': true,
+    '*': false
+  },
+  ReviewController: {
+    'find': true,
+    'findOne': true,
+    '*': false
+  },
   SchoolController: {
     'find': true,
     'findOne': true,
     '*': false
   },
-
+  SeedController: {
+    '*': true
+  },
+  TagController: {
+    'find': true,
+    'findOne': true,
+    '*': false
+  },
+  UserController: {
+    'find': true,
+    'findOne': true,
+    '*': false
+  },
 };
