3c89a97c9361bb591bd577235429e95d10ce3562
paladinze <paladinze@hotmail.com>
2017-01-10 13:08:59 -0500

    disabled bluebird warning

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -4,14 +4,16 @@
  *
  * An asynchronous bootstrap function that runs before your Sails app gets lifted.
  * This gives you an opportunity to set up your data model, run jobs, or perform some special logic.
- *
- * For more information on bootstrapping your app, check out:
- * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
-
+const Promise = require('sequelize').Promise;
 
 module.exports.bootstrap = function(cb) {
+
+  //bluebird promises
+  Promise.config({
+      warnings: false,
+  });
 
   //need this otherwise won't return
   cb();
