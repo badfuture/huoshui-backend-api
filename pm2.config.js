220d57056a74629517d2ed1a4efa5cfe0abf7cfd
paladinze <paladinze@hotmail.com>
2017-02-13 21:00:16 -0500

    modified pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -9,7 +9,6 @@ module.exports = {
       script: "app.js",
       instances: 0,
       exec_mode: "cluster",
-      args: "--prod",
       watch: "true",
       max_memory_restart: "500M",
       error_file: "logs/error.log",
@@ -32,17 +31,6 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
   deploy : {
-    production : {
-      user : "deploy",
-      host : "api.huoshui.tk",
-      ref  : "origin/master",
-      repo : "https://github.com/badfuture/huoshui-backend-api.git",
-      path : "/home/deploy/huoshui_api",
-      "post-deploy" : "npm install && pm2 startOrRestart pm2.config.js --env prod",
-      env  : {
-        NODE_ENV: "production"
-      }
-    },
     dev : {
       user : "ubuntu",
       host : "api.huoshui.tk",
@@ -52,6 +40,17 @@ module.exports = {
       "post-deploy" : "npm install && pm2 startOrRestart pm2.config.js --env dev",
       env  : {
         NODE_ENV: "development"
+      }
+    },
+    production : {
+      user : "deploy",
+      host : "api.huoshui.tk",
+      ref  : "origin/master",
+      repo : "https://github.com/badfuture/huoshui-backend-api.git",
+      path : "/home/deploy/huoshui_api",
+      "post-deploy" : "npm install && pm2 startOrRestart pm2.config.js --env prod",
+      env  : {
+        NODE_ENV: "production"
       }
     }
   }
