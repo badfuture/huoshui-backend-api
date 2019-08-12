021b42dcfe2f8822497f95abd1ca2c9b9ab5164e
paladinze <paladinze@hotmail.com>
2019-08-12 10:39:23 +0800

    removed swagger route

--- config/routes.js
+++ config/routes.js
@@ -2,36 +2,35 @@
  * Route Mappings
  */
 
-var indexRoute = require('../api/routes/indexRoute');
-var authRoute = require('../api/routes/authRoute');
-var commentRoute = require('../api/routes/commentRoute');
-var kelistRoute = require('../api/routes/kelistRoute');
-var courseRoute = require('../api/routes/courseRoute');
-var deptRoute = require('../api/routes/deptRoute');
-var positionRoute = require('../api/routes/positionRoute');
-var profRoute = require('../api/routes/profRoute');
-var reviewRoute = require('../api/routes/reviewRoute');
-var schoolRoute = require('../api/routes/schoolRoute');
-var swaggerRoute = require('../api/routes/swaggerRoute');
-var tagRoute = require('../api/routes/tagRoute');
-var roleRoute = require('../api/routes/roleRoute');
-var userRoute = require('../api/routes/userRoute');
-var feedbackRoute = require('../api/routes/feedbackRoute');
-var searchRoute = require('../api/routes/searchRoute');
-var statRoute = require('../api/routes/statRoute');
-var jobRoute = require('../api/routes/jobRoute');
-var otherRoute = require('../api/routes/otherRoute');
+var indexRoute = require("../api/routes/indexRoute");
+var authRoute = require("../api/routes/authRoute");
+var commentRoute = require("../api/routes/commentRoute");
+var kelistRoute = require("../api/routes/kelistRoute");
+var courseRoute = require("../api/routes/courseRoute");
+var deptRoute = require("../api/routes/deptRoute");
+var positionRoute = require("../api/routes/positionRoute");
+var profRoute = require("../api/routes/profRoute");
+var reviewRoute = require("../api/routes/reviewRoute");
+var schoolRoute = require("../api/routes/schoolRoute");
+var tagRoute = require("../api/routes/tagRoute");
+var roleRoute = require("../api/routes/roleRoute");
+var userRoute = require("../api/routes/userRoute");
+var feedbackRoute = require("../api/routes/feedbackRoute");
+var searchRoute = require("../api/routes/searchRoute");
+var statRoute = require("../api/routes/statRoute");
+var jobRoute = require("../api/routes/jobRoute");
+var otherRoute = require("../api/routes/otherRoute");
 
 // Dynamic include custom routes from api
-var routes = function (routes) {
+var routes = function(routes) {
   var obj = {};
-  routes.forEach(function (route) {
+  routes.forEach(function(route) {
     for (path in route) {
       obj[path] = route[path];
     }
-  })
+  });
   return obj;
-}
+};
 
 module.exports.routes = routes([
   indexRoute,
